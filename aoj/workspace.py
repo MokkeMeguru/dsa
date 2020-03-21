dc94040b7559b0e6118c299aa2538bac068540d8
MokkeMeguru <meguru.mokke@gmail.com>
2020-03-21 14:15:39 +0900

    first commit

--- aoj/workspace.py
+++ aoj/workspace.py
@@ -1,30 +1,58 @@
 import sys
+import math
 input = sys.stdin.readline
+import itertools
+
+
+def solve(i, last):
+    res = 0
+    print(i)
+    while len(last) > 7:
+        print(last[0:7])
+        if sum(last[0:7]) > 5:
+            break
+        else:
+            res += 7
+            last = last[7:]
+
+    if len(last) == 1:
+        if last[0] == 1:
+            res += 0
+        # else:
+        #     res += 1
+    elif len(last) > 7:
+        pass
+    else:
+        if sum(last) == len(last):
+            res += 0
+        else:
+            res += len(last)
+    for j in reversed(last):
+        if j == 0:
+            res -= 1
+    return res - i
 
 def main():
-    H, W, N = map(int, input().rstrip().split())
-    table = []
-    for i in range(H):
-        table.append([0 for w in range(W)])
-    for _ in range(N):
-        h, w, x = map(int, input().rstrip().split())
-        min_h = 0
-        for _h in range(H - 1, -1, -1):
-            if sum([table[_h][x + j] for j in range(w)])== 0:
-                min_h = _h
-            else:
-                break
-        for i in range(h):
-            for j in range(w):
-                table[min_h + i][x + j] = 1
-    for row in reversed(table):
-        for i in row:
-            if i == 0:
-                print(".", end="")
-            else:
-                print("#", end="")
-        print()
-
+    N  = int(input().rstrip())
+    days = list(map(int, input().rstrip().split()))
+    candidates = [(i, [0] * i + days) for i in range(7)]
+    print(max(map(lambda candidate: solve(*candidate), candidates)))
 
 if __name__ == "__main__":
     main()
+
+# / / / / 1 1 1
+# 1 0 1 1 0 1 1
+# 1 / / / / / /
+
+
+
+# 1 1 1 0 1 1 1 0 1 1 0
+
+11
+1 1 1 0 1 1 1 0 1 1 0
+-> 10
+
+/ / / / / / 1
+1 1 0 1 1 1 0
+1 1 0 _ _ _ _
